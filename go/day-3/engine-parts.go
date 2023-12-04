051303f1bee0fc9ef696be7f5e008e8a71ee807e
Camden Brown <camdenebrown@gmail.com>
2023-12-04 14:49:38 -0600

    wip

--- go/day-3/engine-parts.go
+++ go/day-3/engine-parts.go
@@ -10,42 +10,45 @@ import (
 )
 
 func main() {
-	var prevLineSymbolsMap = map[int]rune{}
 	currentNumber := ""
+	var currentLine, previousLine string
+	var currentNumberIndexes []int
+	var numsAdd []int
 	result := 0
+	lineCounter := 0
 	file := openFile()
 	scanner := bufio.NewScanner(file)
 
 	defer file.Close()
 
 	for scanner.Scan() {
-		line := scanner.Text()
+		previousLine = currentLine
+		currentLine = scanner.Text()
 
-		for lineIndex, r := range line {
-			if unicode.IsSymbol(r) || r == '*' || r == '#' {
-				prevLineSymbolsMap[lineIndex] = r
-				fmt.Println(unicode.IsPunct(r), string(r), currentNumber)
+		for lineIndex, r := range currentLine {
+			if IsSymbol(r) {
 				if lineIndex > 0 {
-					var prevIndexValue rune = rune(line[lineIndex-1])
+					var prevIndexValue rune = rune(currentLine[lineIndex-1])
 					if unicode.IsDigit(rune(prevIndexValue)) {
 						digit, err := strconv.Atoi(currentNumber)
 						if err != nil {
 							fmt.Println(err)
 						}
-
+						numsAdd = append(numsAdd, digit)
 						result += digit
 					}
 				}
-				if lineIndex < len(line)-1 {
-					var nextIndexValue rune = rune(line[lineIndex+1])
+				if lineIndex < len(currentLine)-1 {
+					var nextIndexValue rune = rune(currentLine[lineIndex+1])
 					if unicode.IsDigit(rune(nextIndexValue)) {
-						slice := []rune(line)[:lineIndex]
+						slice := []rune(currentLine)[:lineIndex]
 						for _, r := range slice {
 							if !unicode.IsDigit(r) {
 								digit, err := strconv.Atoi(currentNumber)
 								if err != nil {
 									fmt.Println(err)
 								}
+								numsAdd = append(numsAdd, digit)
 								result += digit
 								break
 							}
@@ -58,12 +61,31 @@ func main() {
 
 			if unicode.IsDigit(r) {
 				currentNumber = currentNumber + string(r)
+				currentNumberIndexes = append(currentNumberIndexes, lineIndex)
 			} else {
+				if currentNumber != "" && previousLine != "" {
+					for _, val := range currentNumberIndexes {
+						digit, err := strconv.Atoi(currentNumber)
+						if err != nil {
+							fmt.Println(err)
+						}
+
+						if IsSymbol(rune(previousLine[val])) || (val > 0 && IsSymbol(rune(previousLine[val-1]))) || IsSymbol(rune(previousLine[val+1])) {
+
+							numsAdd = append(numsAdd, digit)
+
+							result += digit
+							break
+						}
+					}
+				}
 				currentNumber = ""
 			}
 		}
+		currentNumberIndexes = make([]int, 0)
+		lineCounter++
 	}
-	fmt.Println("The result is:", result)
+	fmt.Println("The result is:", result, numsAdd)
 }
 
 func openFile() *os.File {
@@ -74,4 +96,8 @@ func openFile() *os.File {
 	}
 
 	return file
+}
+
+func IsSymbol(r rune) bool {
+	return unicode.IsSymbol(r) || r == '*' || r == '#'
 }
