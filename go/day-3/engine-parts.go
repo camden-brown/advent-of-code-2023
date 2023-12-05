67fcf0d7f20ff67101f7459ae9d0c43032bfae40
Camden Brown <camdenebrown@gmail.com>
2023-12-05 11:57:08 -0600

    I SOLVED IT!!!!!!!!!!!!

--- go/day-3/engine-parts.go
+++ go/day-3/engine-parts.go
@@ -9,14 +9,35 @@ import (
 	"unicode"
 )
 
-var currentNumber string = ""
+//Array of Number struct
+//Iterate through line
+//If digit, create Number struct with value, start index, line
+//Once no longer digit, add end index to Number struct
+//Push Number struct to array
+
+//Iterate through array of Number structs
+
+type Number struct {
+	value      string
+	startIndex int
+	endIndex   int
+	line       int
+	consumed   bool
+}
+
+type Symbol struct {
+	index int
+	line  int
+}
+
+var currentNumberStruct Number
+var foundNumbers []Number
+var foundSymbols []Symbol
+var lineCounter int = 0
 var currentLine, previousLine string
 var result int = 0
-var numsAdd []int
-var currentNumberIndexes []int
 
 func main() {
-	// var previousLineNumbersByIndex = map[int][]int{}
 	file := openFile()
 	scanner := bufio.NewScanner(file)
 
@@ -26,26 +47,78 @@ func main() {
 		previousLine = currentLine
 		currentLine = scanner.Text()
 
-		for lineIndex, r := range currentLine {
-			if IsSymbol(r) {
-				symbolAdjacent(lineIndex)
+		for i, r := range currentLine {
+			if isSpecialCharacter(r) {
+				symbolStruct := Symbol{index: i, line: lineCounter}
+				foundSymbols = append(foundSymbols, symbolStruct)
 			}
 
 			if unicode.IsDigit(r) {
-				currentNumber = currentNumber + string(r)
-				currentNumberIndexes = append(currentNumberIndexes, lineIndex)
-			} else {
-				symbolDiagonalToDigit()
-				currentNumber = ""
+				if currentNumberStruct != (Number{}) {
+					currentNumberStruct.endIndex = i
+					currentNumberStruct.value = currentNumberStruct.value + string(r)
+				} else {
+					currentNumberStruct = Number{value: string(r), startIndex: i, endIndex: i, line: lineCounter, consumed: false}
+				}
+
+				if i+1 >= len(currentLine) || !unicode.IsDigit(rune(currentLine[i+1])) {
+					foundNumbers = append(foundNumbers, currentNumberStruct)
+					currentNumberStruct = Number{}
+				}
 			}
 		}
-		currentNumberIndexes = make([]int, 0)
+		lineCounter++
 	}
-	fmt.Println("The result is:", result, numsAdd)
+
+	for i := range foundNumbers {
+		val := &foundNumbers[i]
+		for _, symbol := range foundSymbols {
+			if symbol.line == val.line && val.consumed == false {
+				if symbol.index == val.startIndex-1 || symbol.index == val.endIndex+1 {
+					digit, err := strconv.Atoi(val.value)
+					if err != nil {
+						fmt.Println(err)
+					}
+					val.consumed = true
+					result += digit
+				}
+			}
+
+			if symbol.line == val.line-1 && val.consumed == false {
+				if isInRange(symbol.index, val.startIndex-1, val.endIndex+1) {
+					digit, err := strconv.Atoi(val.value)
+					if err != nil {
+						fmt.Println(err)
+					}
+					val.consumed = true
+					result += digit
+				}
+			}
+
+			if symbol.line == val.line+1 && val.consumed == false {
+				if isInRange(symbol.index, val.startIndex-1, val.endIndex+1) {
+					digit, err := strconv.Atoi(val.value)
+					if err != nil {
+						fmt.Println(err)
+					}
+
+					val.consumed = true
+					result += digit
+				}
+			}
+		}
+	}
+	// fmt.Println("The result is:", result, foundNumbers)
+	// fmt.Println("The result is:", result, foundSymbols)
+	fmt.Println("The result is:", result)
+}
+
+func isInRange(n, start, end int) bool {
+	return n >= start && n <= end
 }
 
 func openFile() *os.File {
-	file, err := os.Open("sample.txt")
+	file, err := os.Open("input.txt")
 
 	if err != nil {
 		log.Fatal(err)
@@ -54,58 +127,12 @@ func openFile() *os.File {
 	return file
 }
 
-func IsSymbol(r rune) bool {
-	return unicode.IsSymbol(r) || r == '*' || r == '#'
-}
-
-func symbolDiagonalToDigit() {
-	if currentNumber != "" && previousLine != "" {
-		for _, val := range currentNumberIndexes {
-			digit, err := strconv.Atoi(currentNumber)
-			if err != nil {
-				fmt.Println(err)
-			}
-
-			if IsSymbol(rune(previousLine[val])) || (val > 0 && IsSymbol(rune(previousLine[val-1]))) || IsSymbol(rune(previousLine[val+1])) {
-
-				numsAdd = append(numsAdd, digit)
-
-				result += digit
-				break
-			}
+func isSpecialCharacter(r rune) bool {
+	specialCharacters := []rune{'*', '+', '=', '&', '%', '$', '@', '#', '-', '/', '&'}
+	for _, c := range specialCharacters {
+		if r == c {
+			return true
 		}
 	}
-}
-
-func symbolAdjacent(lineIndex int) {
-	if lineIndex > 0 {
-		var prevIndexValue rune = rune(currentLine[lineIndex-1])
-		if unicode.IsDigit(rune(prevIndexValue)) {
-			digit, err := strconv.Atoi(currentNumber)
-			if err != nil {
-				fmt.Println(err)
-			}
-			numsAdd = append(numsAdd, digit)
-			result += digit
-		}
-	}
-
-	if lineIndex < len(currentLine)-1 {
-		var nextIndexValue rune = rune(currentLine[lineIndex+1])
-		if unicode.IsDigit(rune(nextIndexValue)) {
-			slice := []rune(currentLine)[:lineIndex]
-			for _, r := range slice {
-				if !unicode.IsDigit(r) {
-					digit, err := strconv.Atoi(currentNumber)
-					if err != nil {
-						fmt.Println(err)
-					}
-					numsAdd = append(numsAdd, digit)
-					result += digit
-					break
-				}
-				currentNumber = currentNumber + string(r)
-			}
-		}
-	}
+	return false
 }
